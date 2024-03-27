2ba73294efb25e9bcf751ea0d6ad46203508f1d1
Mohd Ashraf <122676159+Ashraf1395@users.noreply.github.com>
2024-03-27 06:33:17 +0000

    Update from local

--- commands.sh
+++ commands.sh
@@ -1,8 +1,31 @@
 PROJECT_NAME='ashraf-magic'
 
+# Check if the network exists; if not, create it
+if ! docker network inspect ${PROJECT_NAME}-network &>/dev/null; then
+    docker network create ${PROJECT_NAME}-network
+else
+    echo "Network ${PROJECT_NAME}-network already exists."
+fi
+
+# Start Kafka Containers
+start-kafka(){
+    docker-compose -f docker/kafka/docker-compose.yml up -d
+}
+
+
+# Start Mage Containers
+start-mage(){
+    docker-compose -f docker/mage/docker-compose.yml up -d
+}
+
+#Start producing stream data
+stream-data(){
+    docker-compose -f docker/streaming/docker-compose.yml up 
+}
 gitting(){
     git add .
     git commit -m "Update from local"
     sleep 2
     git push -u origin main
 }
+
---
+++ streaming_pipeline/source_to_kafka/config.py
@@ -0,0 +1,5 @@
+# import pyspark.sql.types as T
+
+BOOTSTRAP_SERVERS = 'kafka:29092'
+TOPIC = 'customer_activity_data'
+FILE_PATH = './data/train.csv'
